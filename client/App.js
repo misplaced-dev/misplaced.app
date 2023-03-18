66c6b3f12e25508abf9b662ef03983f19dd2b4bb
Dawit A <dndawita@gmail.com>
2023-03-17 23:22:17 -0400

    updated

--- client/App.js
+++ client/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>poop</Text>
+      <Text>Start</Text>
       <StatusBar style="auto" />
     </View>
   );
