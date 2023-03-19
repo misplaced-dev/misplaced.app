7cad795c7a99d697c943df5dab8f2f84590bd4f9
Dawit A <dndawita@gmail.com>
2023-03-19 13:30:50 -0400

    template

--- client/src/constants/env.js
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const API_URL = process.env.REACT_APP_API_URL;
-export const REACT_APP_GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
