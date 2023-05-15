c8241977817e04bef8a12a2d125339e6c23a151e
Dawit A <dndawita@gmail.com>
2023-05-15 13:03:19 -0400

    fixed componenet error

--- client/components/Classstuff.jsx
+++ client/components/Classstuff.jsx
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity, TextInput, View, StyleSheet } from 'react-nativ
 
 
 
-const Footer = () => {
+const Classstuff = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
@@ -27,41 +27,38 @@ const Footer = () => {
   return (
     <View style={styles.container}>
         <Text>HTML Table</Text>
-      <table style={styles.table}>
-        <thead>
-          <tr>
-          <th style={styles.cell}>Tech</th>
-            <th style={styles.cell}>Events</th>
-            <th style={styles.cell}>Plans</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td style={styles.cell}>MERN Stack</td>
-            <td style={styles.cell}>Started Spring Break to self-teach React Native</td>
-            <td style={styles.cell}>Pass code audit by Towson U IT dept.</td>
-          </tr>
-          <tr>
-            <td style={styles.cell}>Javascript</td>
-            <td style={styles.cell}>Went to final round in competiton and lost</td>
-            <td style={styles.cell}>Get Misplaced to other universities</td>
-          </tr>
-          <tr>
-            <td style={styles.cell}>Cloudinary</td>
-            <td style={styles.cell}>Had first meeting with Towson VP to establish Misplaced at Towson</td>
-            <td style={styles.cell}>Get a good job at a big tech company and forget about this (jk)</td>
-          </tr>
-        </tbody>
-      </table>
+        <View style={styles.table}>
+      <View style={styles.row}>
+        <Text style={styles.cellb}>Tech</Text>
+        <Text style={styles.cellb}>Events</Text>
+        <Text style={styles.cellb}>Plans</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.cell}>MERN Stack</Text>
+        <Text style={styles.cell}>Started Spring Break to self-teach React Native</Text>
+        <Text style={styles.cell}>Pass code audit by Towson U IT dept.</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.cell}>Javascript</Text>
+        <Text style={styles.cell}>Went to final round in competition and lost</Text>
+        <Text style={styles.cell}>Get Misplaced to other universities</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.cell}>Cloudinary</Text>
+        <Text style={styles.cell}>Had first meeting with Towson VP to establish Misplaced at Towson</Text>
+        <Text style={styles.cell}>Get a good job at a big tech company and forget about this (jk)</Text>
+      </View>
+    </View>
+
       <Text style={{marginTop:100, marginBottom:5,}}>HTML Form</Text>
        <Text>Name:</Text>
-      <TextInput style={{border: '3px solid #ccc', borderRadius: 5}} value={name} onChangeText={setName} />
+      <TextInput style={{border: '3px solid #ccc', borderRadius: 5, padding:3}} value={name} onChangeText={setName} />
 
       <Text>Email:</Text>
-      <TextInput style={{border: '3px solid #ccc', borderRadius: 5}} value={email} onChangeText={setEmail} />
+      <TextInput style={{border: '3px solid #ccc', borderRadius: 5, padding:3}} value={email} onChangeText={setEmail} />
 
       <Text>Message:</Text>
-      <TextInput style={{border: '3px solid #ccc', borderRadius: 5}} value={message} onChangeText={setMessage} multiline />
+      <TextInput style={{border: '3px solid #ccc', borderRadius: 5, padding:3}} value={message} onChangeText={setMessage} multiline />
 
       <TouchableOpacity onPress={handleSubmit}>
         <Text style={{border: '2px solid #ccc', borderRadius: 5, width: 60, marginTop:5, paddingHorizontal: 5, }}>Submit</Text>
@@ -83,15 +80,38 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   table: {
+    display: 'flex',
+    flexDirection: 'column',
     borderCollapse: 'collapse',
     width: '100%',
     marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    border: '2px solid black',
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
   },
   cell: {
     border: '2px solid black',
-    padding: 8,
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 10,
+    width: '33.335%',
+    textAlign: 'center',
   },
-
+  cellb: {
+    border: '2px solid black',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 10,
+    width: '33.335%',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+ 
 });
 
-export default Footer;
+export default Classstuff;
