45c711bc392146b5f780fb003a7b59a509be9baa
Dawit A <dndawita@gmail.com>
2023-05-16 21:16:18 -0400

    used one function in api to evade bills

--- api/call.js
+++ api/call.js
@@ -1,3 +1,7 @@
-import { Server } from '../server/src/server.js';
-
-Server.start();
+import('../server/src/server.js').then(({ Server }) => {
+    Server.start();
+  }).catch((error) => {
+    // Handle any errors that occur during the import
+    console.error('Error while importing server:', error);
+  });
+  
